891a362aba73b8bb17052ff4ed7564bc0e075eeb
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2020-05-31 16:02:46 +0200

    command line

---
+++ bin/steam-totp.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const { Command } = require('commander')
+const totp = require('../lib/totp')
+
+const program = new Command();
+
+program.version(require('../package').version);
+
+program.name('steam totp');
+
+program.option('<secret>', 'Secret code')
+
+program.parse(process.argv)
+
+if (program.args) {
+    const code = totp.GetCode(program.args[0])
+    console.log(code);
+}
