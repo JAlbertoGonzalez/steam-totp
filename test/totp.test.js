9437d1ed452908686b30e4983306ee7cb2893046
Alberto Torre <45895462+JAlbertoGonzalez@users.noreply.github.com>
2023-03-23 15:04:22 +0100

    upgrade deps + migrate to jest + migrate to yarn

--- test/totp.test.js
+++ test/totp.test.js
@@ -1,4 +1,3 @@
-const { expect } = require('chai')
 const { GetCode } = require('../lib/totp')
 const os = require('os')
 
@@ -9,7 +8,7 @@ const TimeTest = 1590922625587; // Test are based on this date
 describe('totp', () => {
     it('should get correct code', () => {
         const code = GetCode(SecretTest, TimeTest);
-        expect(code).equals('F9R52');
+        expect(code).toEqual('F9R52');
     })
 
     it('should parse date as Date and epoch unix timestamp', () => {
@@ -19,13 +18,13 @@ describe('totp', () => {
         const code1 = GetCode(SecretTest, TimeTestDate);
         const code2 = GetCode(SecretTest, UnixTimestamp);
 
-        expect(code1).equals(code2);
+        expect(code1).toEqual(code2);
     })
 
     it('should always use little endian', () => {
         if (os.endianness() === 'BE') {
             const code = GetCode(SecretTest, TimeTest);
-            expect(code).equals('F9R52');
+            expect(code).toEqual('F9R52');
         }
     })
 
@@ -33,6 +32,6 @@ describe('totp', () => {
         const CurrentTime = new Date();
         const code1 = GetCode(SecretTest, CurrentTime);
         const code2 = GetCode(SecretTest);
-        expect(code1).equals(code2);
+        expect(code1).toEqual(code2);
     })
 })
